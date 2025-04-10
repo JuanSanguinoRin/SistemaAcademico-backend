142c55c5b230f6ae207f7418f286780fe8259c91
JuanSanguinoRin <130527480+JuanSanguinoRin@users.noreply.github.com>
2025-04-10 16:24:23 -0500

    pito

--- backend/src/main/java/co/edu/ufps/backend/service/HorarioAdministrativoService.java
+++ backend/src/main/java/co/edu/ufps/backend/service/HorarioAdministrativoService.java
@@ -5,7 +5,6 @@ import co.edu.ufps.backend.repository.HorarioAdministrativoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
