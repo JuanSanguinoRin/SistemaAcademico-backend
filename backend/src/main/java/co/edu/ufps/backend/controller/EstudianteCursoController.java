cfd96e9c827238ce46c93e56dc8a7136421d65f1
JuanPlay27 <juandavid_mr27@hotmail.com>
2025-04-11 17:10:51 -0500

    si compila
    al menos

--- backend/src/main/java/co/edu/ufps/backend/controller/EstudianteCursoController.java
+++ backend/src/main/java/co/edu/ufps/backend/controller/EstudianteCursoController.java
@@ -73,15 +73,7 @@ public class EstudianteCursoController {
     }
 
 
-    @GetMapping("/{id}/definitiva")
-    public ResponseEntity<?> calcularDefinitiva(@PathVariable Long id) {
-        try {
-            Float nota = estudianteCursoService.calcularDefinitiva(id);
-            return ResponseEntity.ok(Map.of("definitiva", nota));
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
+
 
     @GetMapping("/{id}/habilitacion")
     public ResponseEntity<?> comprobarRehabilitacion(@PathVariable Long id) {
@@ -103,15 +95,5 @@ public class EstudianteCursoController {
         }
     }
 
-    @PostMapping("/matricular/{estudianteId}/{cursoId}")
-    public ResponseEntity<?> matricularCurso(
-            @PathVariable Long estudianteId,
-            @PathVariable Long cursoId) {
-        try {
-            EstudianteCurso ec = estudianteCursoService.matricularCurso(estudianteId, cursoId);
-            return ResponseEntity.ok(ec);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
+
 }
